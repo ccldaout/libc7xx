69d4208a6d6191e4d689c86173bc43d758e1d11d
Masakazu Iwanaga <ccldaout@gmail.com>
2022-03-06 17:14:15 +0900

    c7event/submit

---
+++ src/c7event/submit.cpp
@@ -0,0 +1,100 @@
+/*
+ * c7event/submit.cpp
+ *
+ * Copyright (c) 2021 ccldaout@gmail.com
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+
+#include <c7event/submit.hpp>
+#include <sys/eventfd.h>
+
+
+namespace c7::event {
+
+
+c7::result<std::shared_ptr<submit_provider>>
+submit_provider::make()
+{
+    auto sp = std::shared_ptr<submit_provider>(new submit_provider());
+    if (auto res = sp->init(); !res) {
+	return res.as_error();
+    }
+    return c7result_ok(sp);
+}
+
+
+c7::result<std::shared_ptr<submit_provider>>
+submit_provider::make_and_manage()
+{
+    return make_and_manage(c7::event::default_event_monitor());
+}
+
+
+c7::result<std::shared_ptr<submit_provider>>
+submit_provider::make_and_manage(c7::event::monitor& mon)
+{
+    if (auto res = make(); !res) {
+	return res;
+    } else {
+	auto sp = std::move(res.value());
+	if (auto res = mon.manage(manage_key, sp); !res) {
+	    return res.as_error();
+	}
+	return c7result_ok(sp);
+    }
+}
+
+
+c7::result<>
+submit_provider::init()
+{
+    int fd = ::eventfd(0, EFD_CLOEXEC);
+    if (fd == C7_SYSERR) {
+	return c7result_err(errno, "Failed to create eventfd.");
+    }
+    evfd_ = c7::fd(fd);
+    return c7result_ok();
+}
+
+
+void
+submit_provider::on_event(c7::event::monitor&, int prvfd, uint32_t events)
+{
+    uint64_t submit_count;
+    if (auto io_res = evfd_.read_n(&submit_count); !io_res) {
+	auto res = c7result_err(std::move(io_res.get_result()), "Failed to read eventfd.");
+	on_error(res);
+	return;
+    }
+    for (uint64_t i = 0; i < submit_count; i++) {
+	if (auto q_res = callbacks_.get(0); !q_res) {
+	    auto res = c7result_err(std::move(q_res.as_error()),
+				    "rest submit count:%{} (from eventfd)", submit_count - 1);
+	    on_error(res);
+	} else {
+	    q_res.value()();
+	}
+    }
+}
+
+
+c7::result<>
+submit_provider::submit(std::function<void()>&& f)
+{
+    if (auto res = callbacks_.put(std::move(f)); !res) {
+	return res;
+    }
+    uint64_t submit_count = 1;
+    if (auto io_res = evfd_.write_n(&submit_count); !io_res) {
+	auto res = c7result_err(std::move(io_res.get_result()), "Failed to write eventfd.");
+	return res;
+    }
+    return c7result_ok();
+}
+
+
+} // namespae c7::event
+
---
+++ src/c7event/submit.hpp
@@ -0,0 +1,54 @@
+/*
+ * c7event/submit.hpp
+ *
+ * Copyright (c) 2021 ccldaout@gmail.com
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * Google spreadsheets:
+ * (Nothing)
+ */
+#ifndef C7_EVENT_SUBMIT_HPP_LOADED__
+#define C7_EVENT_SUBMIT_HPP_LOADED__
+#include <c7common.hpp>
+
+
+#include <c7event/monitor.hpp>
+#include <c7fd.hpp>
+#include <c7thread/queue.hpp>
+
+
+namespace c7::event {
+
+
+class submit_provider: public c7::event::provider_interface {
+public:
+    static constexpr const char * const manage_key = "jbx.ipc.submit_provider";
+
+    c7::delegate<void, c7::result_base&> on_error;
+
+    static c7::result<std::shared_ptr<submit_provider>> make();
+    static c7::result<std::shared_ptr<submit_provider>> make_and_manage();
+    static c7::result<std::shared_ptr<submit_provider>> make_and_manage(c7::event::monitor&);
+
+    ~submit_provider() override = default;
+    int fd() override { return evfd_; }
+    void on_event(c7::event::monitor&, int prvfd, uint32_t events) override;
+
+    c7::result<> submit(std::function<void()>&& f);
+
+private:
+    c7::fd evfd_;
+    c7::thread::queue<std::function<void()>> callbacks_;
+
+    submit_provider() = default;
+    c7::result<> init();
+};
+
+
+} // namespace c7::event
+
+
+#endif // c7event/submit.hpp
+
